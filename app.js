f5afe19c68a6c6aff8d07f7ab43c031a4d6e8451
griffith <griffithawuah15@gmail.com>
2018-11-18 21:05:58 +0000

    implemented tweeet fnx

--- app.js
+++ app.js
@@ -1,8 +1,19 @@
-const express = require('express');
+const sherperd = require('./lib/sherperd');
 const bodyParser = require('body-parser');
+const express = require('express');
 const logger = require('morgan');
 const cors = require('cors');
 require('dotenv').config();
+
+function buildJobPosting(body) {
+  /* 
+    we build a well formatted job 
+    using input provided, but for
+    now, just return body.payload
+  */
+
+  return body.payload || 'Hello World. :)'
+}
 
 const app = express();
 
@@ -13,11 +24,33 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 // use api routes
-
 app.get('/', (req, res) => {
   return res.status(200).json({app_name: 'Job2Twitter'})
 });
 
+app.post('/api/webhook', async (req, res, next) => {
+  const {body} = req;
+
+  console.log('Well what do we have here', body);
+
+  try {
+    const resp = await sherperd.tweet(buildJobPosting(body)); 
+  } catch (error) {
+    return next(error)
+  }
+
+  console.log('Posting was succesful');
+
+  return res.status(200).send(1)
+
+});
+
+app.use((err, req, res, next) => {
+  console.log(err);
+  res.status(500).send(0)
+});
+
+
 app.listen(2019, () => {
   console.log('🍁 running successfully on :2019')
-})
+});
